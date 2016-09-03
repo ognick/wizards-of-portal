2d943f83e1f4234d3d08b44f8faff66026708e7e
ognick <ogneslav.work@gmail.com>
2016-09-03 19:51:31 +0300

    First try AI solve

---
+++ src/main/scala/RunSolver.scala
@@ -0,0 +1,52 @@
+import wop.game.{Point, WopState}
+import wop.game.WopState.{InProgress, Player}
+import wop.game.ai.WopSolver
+
+import scala.annotation.tailrec
+
+object RunSolver extends App {
+  @tailrec def loop(state: WopState): Option[WopState] = {
+    println(state)
+    state match {
+      case s: InProgress if s.player == Player.P2 =>
+        WopSolver.alphaBeta(Right(state), None, 0) match {
+          case (eval, Some(p)) => {
+            println("Solve", eval, p)
+            s(p) match {
+              case Right(newState) => loop(newState)
+              case x => {
+                println("i dont know", x)
+                None
+              }
+            }
+          }
+          case x => {
+            println("i dont know", x)
+            None
+          }
+        }
+      case prevState: WopState.Turn =>
+        println("input local x,y")
+        val point: Point = (readInt(), readInt())
+        prevState(point) match {
+          case Right(newState) => loop(newState)
+          case _ =>
+            println("wrong input")
+            loop(prevState)
+        }
+      case prevState: WopState.Select =>
+        println("input global x,y")
+        val point: Point = (readInt(), readInt())
+        prevState(point) match {
+          case Right(newState) => loop(newState)
+          case _ =>
+            println("wrong input")
+            loop(prevState)
+        }
+      case _ => None
+    }
+  }
+
+  loop(WopState.initial)
+
+}
---
+++ src/test/scala/WopSolverSpec.scala
@@ -0,0 +1,28 @@
+import org.scalatest._
+import wop.game.TicTacToe
+import wop.game.WopState
+import wop.game.WopState.SubBoard
+import wop.game.ai.WopSolver
+
+class WopSolverSpec extends FlatSpec with Matchers {
+
+  it should "be has eval" in {
+    val field = Field(
+      "x_x",
+      "_x_",
+      "___"
+    )
+    WopSolver.heuristicEvalSubBoard(field) should be (13)
+  }
+
+  def Field(xs: String*): SubBoard = {
+    TicTacToe(size = 3,
+      matrix = xs.mkString.toVector map {
+        case 'x' => WopState.XO.X
+        case 'o' => WopState.XO.O
+        case _ => WopState.XO.Empty
+      }
+    )
+  }
+
+}
